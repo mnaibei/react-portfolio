1ba500acdd1e1fda852b887cfc1fa58f4e4d2d3a
mnaibei <muchajulius@gmail.com>
2023-09-04 11:32:46 +0300

    Updated bio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,18 @@ const Home = () => (
     <img src={avatar} alt={avatar} />
     <div className="home-content">
       <h1 className="title">IT specialist, software engineer</h1>
-      <span className="about-me"><p>As an experienced Information Technology Specialist/Consultant, I have been successfully providing IT services for over four years. My expertise lies in establishing and implementing security policies, upgrading existing systems, and integrating new technologies. In addition to my IT experience, I am also a skilled software engineer with a strong focus on delivering high-quality code and innovative solutions. I am dedicated to continuous learning and staying up-to-date with the latest industry trends and technologies. As a software engineer, I thrive in fast-paced, challenging environments and always strive to provide innovative solutions that exceed customer expectations. I possess excellent problem-solving abilities, strong time management skills, and the ability to work effectively both independently and as part of a team</p></span>
+      <span className="about-me">
+        <p>
+          Hello, I am Mucha Naibei, an IT Specialist/Consultant with over six years of experience.
+          I am passionate about enhancing IT security, implementing cutting-edge technologies,
+          and delivering top-notch solutions.
+          Beyond IT, I am a skilled software engineer dedicated to continuous
+          learning and staying updated with industry trends.
+          I thrive in fast-paced environments, solving challenges, and exceeding customer
+          expectations.
+          I bring problem-solving prowess, effective time management, and teamwork to every project.
+        </p>
+      </span>
       <a className="view-resume-button" href="/resume">View Resume</a>
     </div>
   </div>
